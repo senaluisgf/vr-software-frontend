9c553a5fa8523dfaab13d291fa73945bd9576b2c
Luis 'Shadow' Sena <sena.luisgf@gmail.com>
2023-02-05 16:39:24 -0400

    styles: change title name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'VR Software College';
   constructor(private readonly router: Router) {}
 
   toCourses() {
