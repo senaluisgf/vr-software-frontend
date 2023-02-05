ce54fa321bbe7ac65952dbcdb900dbb64822833f
Luis 'Shadow' Sena <sena.luisgf@gmail.com>
2023-02-04 20:12:11 -0400

    feat: create course form component; add angular material button and icon; create stub funcions in courses component

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Course } from '../models/course';
 import { CoursesService } from '../services/courses.service';
@@ -9,13 +10,27 @@ import { CoursesService } from '../services/courses.service';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
-  courses$: Observable< Course[]>;
-  displayedColumns = ['id', 'description', 'courseProgram'];
+  courses$: Observable<Course[]>;
+  displayedColumns = ['id', 'description', 'courseProgram', 'actions'];
 
-  constructor(private service: CoursesService){
+  constructor(
+    private service: CoursesService,
+    private router: Router,
+    private route: ActivatedRoute
+    ) {
     this.courses$ = this.service.loadCourses()
   }
 
 
-  ngOnInit() : void {}
+  ngOnInit(): void { }
+
+  onAdd() {
+    this.router.navigate(['new'], {relativeTo: this.route})
+  }
+  onEdit() {
+    console.log('onEdit')
+  }
+  onDelete() {
+    console.log('onDelete')
+  }
 }
